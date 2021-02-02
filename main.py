e4d8300ea117ae26e7cd03dab98b3c69cfbf7f10
sebass-but-ab <78428559+sebass-but-ab@users.noreply.github.com>
2021-02-02 22:36:54 +0800

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,136 @@
+x = 0
+y = 0
+result = 0
+op = ["+","-","*","/"]
+opi = 0
+opfinal = ""
+opindex = 0
+i = 0
+stage = 0
+
+def add(x,y):
+    global result
+    result = x + y
+    return result
+
+def sub(x,y):
+    global result
+    result = x - y
+    return result
+
+def mul(x,y):
+    global result
+    result = x*y
+    return result
+
+def div(x,y):
+    global result
+    result = x/y
+    return result
+
+def con():
+    global i 
+    if i < 0:
+        i = 0
+    if i > 3:
+        i = 3
+
+
+
+def setop():
+    global opfinal,i,stage
+    
+    con()
+    opfinal = op[i]
+    if stage == 0:
+        basic.show_string(str(opfinal))
+    else:
+        basic.clear_screen()
+    
+        
+        
+    
+    
+    
+
+def opa():
+    global i,x,y
+    if stage ==0:
+        i-=1
+    if stage ==1:
+        x-=1
+    if stage ==2:
+        y-=1
+    else:
+        pass
+input.on_button_pressed(Button.A, opa)
+
+def opb():
+    global i,x,y
+    if stage == 0:
+        i+=1
+    if stage ==1:
+        x+=1
+    if stage == 2:
+        y+=1
+    else:
+        pass
+input.on_button_pressed(Button.B, opb)
+
+def opab():
+    global stage
+    
+    stage +=1
+    basic.show_icon(IconNames.YES)
+    
+    pause(200)
+    basic.clear_screen()
+    
+def input1():
+    global x
+    basic.show_string(str(x))
+    
+    
+    
+def input2():
+    global y
+    basic.show_string(str(y))
+   
+def calc():
+    global x,y,result
+    if opfinal == "+":
+        add(x,y)
+    if opfinal == "-":
+        sub(x,y)
+    if opfinal == "*":
+        mul(x,y)
+    if opfinal == "/":
+        div(x,y)
+
+    
+    
+input.on_button_pressed(Button.AB, opab)
+    
+
+
+def on_forever():
+    if stage == 0:
+        setop()
+
+    if stage == 1:
+        input1()
+    if stage == 2:
+        input2()
+    if stage == 3:
+        
+        calc()
+        pause(500)
+        basic.show_string(x + opfinal + y + "=" + result)
+        pause(10000)
+        control.reset()
+        
+    
+
+forever(on_forever)
+
+    
---
+++ main.ts
@@ -0,0 +1,150 @@
+let x = 0
+let y = 0
+let result = 0
+let op = ["+", "-", "*", "/"]
+let opi = 0
+let opfinal = ""
+let opindex = 0
+let i = 0
+let stage = 0
+function add(x: number, y: number) {
+    
+    result = x + y
+    return result
+}
+
+function sub(x: number, y: number): number {
+    
+    result = x - y
+    return result
+}
+
+function mul(x: number, y: number): number {
+    
+    result = x * y
+    return result
+}
+
+function div(x: number, y: number): number {
+    
+    result = x / y
+    return result
+}
+
+function con() {
+    
+    if (i < 0) {
+        i = 0
+    }
+    
+    if (i > 3) {
+        i = 3
+    }
+    
+}
+
+function setop() {
+    
+    con()
+    opfinal = op[i]
+    if (stage == 0) {
+        basic.showString("" + opfinal)
+    } else {
+        basic.clearScreen()
+    }
+    
+}
+
+input.onButtonPressed(Button.A, function opa() {
+    
+    if (stage == 0) {
+        i -= 1
+    }
+    
+    if (stage == 1) {
+        x -= 1
+    }
+    
+    if (stage == 2) {
+        y -= 1
+    } else {
+        
+    }
+    
+})
+input.onButtonPressed(Button.B, function opb() {
+    
+    if (stage == 0) {
+        i += 1
+    }
+    
+    if (stage == 1) {
+        x += 1
+    }
+    
+    if (stage == 2) {
+        y += 1
+    } else {
+        
+    }
+    
+})
+function input1() {
+    
+    basic.showString("" + x)
+}
+
+function input2() {
+    
+    basic.showString("" + y)
+}
+
+function calc() {
+    
+    if (opfinal == "+") {
+        add(x, y)
+    }
+    
+    if (opfinal == "-") {
+        sub(x, y)
+    }
+    
+    if (opfinal == "*") {
+        mul(x, y)
+    }
+    
+    if (opfinal == "/") {
+        div(x, y)
+    }
+    
+}
+
+input.onButtonPressed(Button.AB, function opab() {
+    
+    stage += 1
+    basic.showIcon(IconNames.Yes)
+    pause(200)
+    basic.clearScreen()
+})
+forever(function on_forever() {
+    if (stage == 0) {
+        setop()
+    }
+    
+    if (stage == 1) {
+        input1()
+    }
+    
+    if (stage == 2) {
+        input2()
+    }
+    
+    if (stage == 3) {
+        calc()
+        pause(500)
+        basic.showString(x + opfinal + y + "=" + result)
+        pause(10000)
+        control.reset()
+    }
+    
+})
